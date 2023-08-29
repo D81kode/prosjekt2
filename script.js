8ceb70f3eed002181131f7c9c6caeb1093204ddc
D81kode <daniel81.kodehode@gmail.com>
2023-08-29 14:32:44 +0200

    Lakt til en JS fil for og leke med console.log

---
+++ script.js
@@ -0,0 +1,15 @@
+let konsollTest = "Hei"
+console.log(konsollTest)
+
+let paycheck = 100
+let accountBallance = 200
+let total = (accountBallance + paycheck)
+
+console.log("Totalen er: "+total)
+console.log("Totalen er: " +(paycheck + accountBallance))
+
+let firstName = "Daniel"
+let lastName = "PadleAame"
+let fullName = `My name is: ${firstName} ${lastName}`
+console.log(fullName)
+
